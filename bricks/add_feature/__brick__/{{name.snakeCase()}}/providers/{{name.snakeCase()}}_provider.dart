aaa442600970fb97b5d273b77deda160bb1d3eba
jemish54 <jemishmavani054@gmail.com>
2023-09-17 14:54:56 +0530

    Fixed Mistake in add_feature

--- bricks/add_feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_provider.dart
+++ bricks/add_feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_provider.dart
@@ -2,7 +2,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 
 final {{name.camelCase()}}StateProvider =
-    {{name.pascalCase()}}NotifierProvider.autoDispose<{{name.pascalCase()}}Notifier, {{name.pascalCase()}}State>({{name.pascalCase()}}Notifier.new);
+    NotifierProvider.autoDispose<{{name.pascalCase()}}Notifier, {{name.pascalCase()}}State>({{name.pascalCase()}}Notifier.new);
 
 class {{name.pascalCase()}}Notifier extends AutoDisposeNotifier<{{name.pascalCase()}}State> {
 
--- bricks/add_feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_state.dart
+++ bricks/add_feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_state.dart
@@ -5,7 +5,7 @@ part '{{name.snakeCase()}}_state.g.dart';
 
 @freezed
 class {{name.pascalCase()}}State with _${{name.pascalCase()}}State {
-  const factory {{name.pascalCase()}}State({}) = _{{name.pascalCase()}}State;
+  const factory {{name.pascalCase()}}State() = _{{name.pascalCase()}}State;
 
   factory {{name.pascalCase()}}State.fromJson(Map<String,dynamic> json) => _${{name.pascalCase()}}StateFromJson(json);
 }
