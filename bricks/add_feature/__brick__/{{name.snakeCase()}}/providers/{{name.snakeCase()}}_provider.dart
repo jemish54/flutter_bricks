52a478874a21e90dec0084657928b6dbd22da583
jemish54 <jemishmavani054@gmail.com>
2023-09-17 15:00:04 +0530

    Fixed Mistake in add_feature

--- bricks/add_feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_provider.dart
+++ bricks/add_feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_provider.dart
@@ -1,8 +1,8 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+import '{{name.snakeCase()}}_state.dart';
 
 final {{name.camelCase()}}StateProvider =
-    {{name.pascalCase()}}NotifierProvider.autoDispose<{{name.pascalCase()}}Notifier, {{name.pascalCase()}}State>({{name.pascalCase()}}Notifier.new);
+    NotifierProvider.autoDispose<{{name.pascalCase()}}Notifier, {{name.pascalCase()}}State>({{name.pascalCase()}}Notifier.new);
 
 class {{name.pascalCase()}}Notifier extends AutoDisposeNotifier<{{name.pascalCase()}}State> {
 
