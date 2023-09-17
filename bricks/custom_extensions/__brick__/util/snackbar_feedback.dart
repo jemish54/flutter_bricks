ae0c47592b9306118c7450cd052863d6fd027a53
jemish54 <jemishmavani054@gmail.com>
2023-09-17 15:11:08 +0530

    Added custom extensions

---
+++ bricks/custom_extensions/__brick__/util/snackbar_feedback.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:score_app/util/context_extensions.dart';
+
+extension SnackBarFeedBack on BuildContext {
+  void showInformation(String text) {
+    ScaffoldMessenger.of(this).hideCurrentSnackBar();
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(
+          text,
+          style: textTheme.labelLarge!.copyWith(color: colors.onSecondary),
+        ),
+        backgroundColor: colors.secondary,
+        margin: const EdgeInsets.all(16),
+        behavior: SnackBarBehavior.floating,
+        elevation: 4,
+      ),
+    );
+  }
+
+  void showSuccess(String text) {
+    ScaffoldMessenger.of(this).hideCurrentSnackBar();
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(
+          text,
+          style: textTheme.labelLarge!.copyWith(color: colors.onBackground),
+        ),
+        backgroundColor:
+            isLightTheme ? Colors.green.shade500 : Colors.greenAccent,
+        margin: const EdgeInsets.all(16),
+        behavior: SnackBarBehavior.floating,
+        elevation: 4,
+      ),
+    );
+  }
+
+  void showError(String text) {
+    ScaffoldMessenger.of(this).hideCurrentSnackBar();
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(
+          text,
+          style: textTheme.labelLarge!.copyWith(color: colors.error),
+        ),
+        backgroundColor: colors.errorContainer,
+        margin: const EdgeInsets.all(16),
+        behavior: SnackBarBehavior.floating,
+        elevation: 4,
+      ),
+    );
+  }
+}
